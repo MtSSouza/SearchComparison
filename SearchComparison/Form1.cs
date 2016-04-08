1328906c1d9293d7379c8459bda6e5a7eb1b39cb
mathino <mathsouza64@gmail.com>
2016-04-08 15:06:06 -0300

    Finished Linear

--- SearchComparison/Form1.cs
+++ SearchComparison/Form1.cs
@@ -13,7 +13,8 @@ namespace SearchComparison
     public partial class Form1 : Form
     {
         public List<int> lista = new List<int>();
-        private const int listCount = 10;
+        private const int listCount = 1000;
+        private int indexSearch;
         public Form1()
         {
             InitializeComponent();
@@ -25,7 +26,7 @@ namespace SearchComparison
             Random rand = new Random();
             for (int i = 0; i < listCount; i++)
             {
-                lista.Add(rand.Next(0, 2000));
+                lista.Add(rand.Next(10000));
             }
             lista.Sort();
             listBox1.Items.Clear();
@@ -37,12 +38,26 @@ namespace SearchComparison
 
         private void button2_Click(object sender, EventArgs e)
         {
-            label1.Text = "";
+           label1.Text = "";
             for(int i = 0; i < lista.Count(); i++)
             {
-                
+                if (lista[i] == int.Parse(textBox1.Text)) 
+                {
+                    label1.Text += "The number has been found in the position" + " " + (i + 1) + "! " + "\n";
+                }
+            }
+            if (label1.Text == "" || label1.Text == "This number has not been found! Please, put another one!")
+            {
+                label1.Text = "This number has not been found! Please, put another one!";
             }
             
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            indexSearch = int.Parse(textBox2.Text);
+            label4.Text = "";
+            label4.Text = "The number in this position is " + lista[indexSearch - 1].ToString();
         }
 
       
